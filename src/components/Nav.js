5a4ca19c41e802948edf3f8f720f8009461b5254
Theresa Newman <theresa.webdev@gmail.com>
2018-10-24 07:52:37 -0400

    add username in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import './Nav.css'
 
- export default function Nav () {
-  return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/new' activeClassName='active'>
-            New Poll
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  )
+class Nav extends Component {
+  render() {
+    return (
+      <nav className='nav'>
+        <ul>
+          <li>
+            <NavLink to='/' exact activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/new' activeClassName='active'>
+              New Poll
+            </NavLink>
+          </li>
+        </ul>
+        <span className="naveUsername">Hi {this.props.authedUser}</span>
+      </nav>
+    )
+  }
 }
+function mapStateToProps ({ authedUser }) {
+ return {
+   authedUser
+ }
+}
+export default connect(mapStateToProps)(Nav)
